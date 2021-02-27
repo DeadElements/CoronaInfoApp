5e4ff29bf85c4b39274bb8156b5f69858831f88c
Michael Richard Kathofer <michaelkathofer@Michaels-MacBook-Pro.local>
2021-02-27 17:28:41 +0100

    Settings save state

--- app/src/main/java/com/example/coronainfoapp/Settings.java
+++ app/src/main/java/com/example/coronainfoapp/Settings.java
@@ -1,9 +1,12 @@
 package com.example.coronainfoapp;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -11,31 +14,92 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
-
-
+import static com.example.coronainfoapp.R.string.notificationTirol;
 
 
 public class Settings extends AppCompatActivity {
 
     String channel = "1111";
+    String derText = "Eine Nachricht";
+
+    SwitchMaterial switchMaterialVO;
+    SwitchMaterial switchMaterialTI;
+    SwitchMaterial switchMaterialSA;
+    SwitchMaterial switchMaterialOOE;
+    SwitchMaterial switchMaterialNOE;
+    SwitchMaterial switchMaterialWI;
+    SwitchMaterial switchMaterialBU;
+    SwitchMaterial switchMaterialST;
+    SwitchMaterial switchMaterialKAE;
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        /*
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);*/
 
-        hideNavigationbar();
+
+        // Vorarlber  ------------------------------------------------------------
+        switchMaterialVO = findViewById(R.id.switchMaterial_1);
+        SharedPreferences settings = getSharedPreferences("Settings", 0);
+        switchMaterialVO.setChecked(settings.getBoolean("Vorarlberg",false));
+        voListener();
+
+        // Tirol ------------------------------------------------------------------
+        switchMaterialTI = findViewById(R.id.switchMaterial_2);
+        switchMaterialTI.setChecked(settings.getBoolean("Tirol", false));
+        tiListener();
+
+        // Salzburg ---------------------------------------------------------------
+        switchMaterialSA = findViewById(R.id.switchMaterial_3);
+        switchMaterialSA.setChecked(settings.getBoolean("Salzburg", false));
+        saListener();
+
+        // Oberösterreich ---------------------------------------------------------
+        switchMaterialOOE = findViewById(R.id.switchMaterial_4);
+        switchMaterialOOE.setChecked(settings.getBoolean("Oberoesterreich", false));
+        ooeListener();
+
+        // Niederösterreich -------------------------------------------------------
+        switchMaterialNOE = findViewById(R.id.switchMaterial_5);
+        switchMaterialNOE.setChecked(settings.getBoolean("Niederoesterreich", false));
+        noeListener();
+
+        // Wien -------------------------------------------------------------------
+        switchMaterialWI = findViewById(R.id.switchMaterial_6);
+        switchMaterialWI.setChecked(settings.getBoolean("Wien", false));
+        wiListener();
+
+        // Burgenland -------------------------------------------------------------------
+        switchMaterialBU = findViewById(R.id.switchMaterial_7);
+        switchMaterialBU.setChecked(settings.getBoolean("Burgenland", false));
+        buListener();
+
+        // Steiermark -------------------------------------------------------------------
+        switchMaterialST = findViewById(R.id.switchMaterial_8);
+        switchMaterialST.setChecked(settings.getBoolean("Steiermark", false));
+        stListener();
+
+        // Kärnten -------------------------------------------------------------------
+        switchMaterialKAE = findViewById(R.id.switchMaterial_9);
+        switchMaterialKAE.setChecked(settings.getBoolean("Kärnten", false));
+        kaeListener();
+
+
 
 
         FloatingActionButton fab = findViewById(R.id.floating_action_button);
@@ -49,39 +113,107 @@ public class Settings extends AppCompatActivity {
             }
         });
 
-        //Notifications ------------------------------
-        createNotificationChannel();
+
+
+
     }
 
-    private void hideNavigationbar() {
-        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                View.SYSTEM_UI_FLAG_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    private void voListener() {
+        switchMaterialVO.setOnClickListener(v -> {
+            SharedPreferences settings1 = getSharedPreferences("Settings", 0);
+            SharedPreferences.Editor editor = settings1.edit();
+            editor.putBoolean("Vorarlberg", switchMaterialVO.isChecked());
+            editor.apply();
+        });
+
+
+            addNotification();
+
+    }
+    private void tiListener() {
+        switchMaterialTI.setOnClickListener(v -> {
+            SharedPreferences settings1 = getSharedPreferences("Settings", 0);
+            SharedPreferences.Editor editor = settings1.edit();
+            editor.putBoolean("Tirol", switchMaterialTI.isChecked());
+            editor.apply();
+        });
+    }
+    private void saListener() {
+        switchMaterialSA.setOnClickListener(v -> {
+            SharedPreferences settings1 = getSharedPreferences("Settings", 0);
+            SharedPreferences.Editor editor = settings1.edit();
+            editor.putBoolean("Salzburg", switchMaterialSA.isChecked());
+            editor.apply();
+        });
+    }
+    private void ooeListener() {
+        switchMaterialOOE.setOnClickListener(v -> {
+            SharedPreferences settings1 = getSharedPreferences("Settings", 0);
+            SharedPreferences.Editor editor = settings1.edit();
+            editor.putBoolean("Oberoesterreich", switchMaterialOOE.isChecked());
+            editor.apply();
+        });
+    }
+    private void noeListener() {
+        switchMaterialNOE.setOnClickListener(v -> {
+            SharedPreferences settings1 = getSharedPreferences("Settings", 0);
+            SharedPreferences.Editor editor = settings1.edit();
+            editor.putBoolean("Niederoesterreich", switchMaterialNOE.isChecked());
+            editor.apply();
+        });
+    }
+    private void wiListener() {
+        switchMaterialWI.setOnClickListener(v -> {
+            SharedPreferences settings1 = getSharedPreferences("Settings", 0);
+            SharedPreferences.Editor editor = settings1.edit();
+            editor.putBoolean("Wien", switchMaterialWI.isChecked());
+            editor.apply();
+        });
+    }
+    private void buListener() {
+        switchMaterialBU.setOnClickListener(v -> {
+            SharedPreferences settings2 = getSharedPreferences("Settings", 0);
+            SharedPreferences.Editor editor = settings2.edit();
+            editor.putBoolean("Burgenland", switchMaterialBU.isChecked());
+            editor.apply();
+        });
+    }
+    private void stListener() {
+        switchMaterialST.setOnClickListener(v -> {
+            SharedPreferences settings3 = getSharedPreferences("Settings", 0);
+            SharedPreferences.Editor editor = settings3.edit();
+            editor.putBoolean("Steiermark", switchMaterialST.isChecked());
+            editor.apply();
+        });
+    }
+    private void kaeListener() {
+        switchMaterialKAE.setOnClickListener(v -> {
+            SharedPreferences settings1 = getSharedPreferences("Settings", 0);
+            SharedPreferences.Editor editor = settings1.edit();
+            editor.putBoolean("Kärnten", switchMaterialKAE.isChecked());
+            editor.apply();
+        });
     }
 
-    ;
 
+    private void addNotification() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.notificationTirol);
-            String description = getString(R.string.notificationTirolBeschreibung);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("1111", name, importance);
-            channel.setDescription(description);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channel)
+                .setSmallIcon(R.drawable.ic_info_solid)
+                .setContentTitle("Voralberg Notifications eingerichtet")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        // The id of the channel.
-        String id = "my_channel_01";
-        notificationManager.deleteNotificationChannel(id);
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1, builder.build());
 
     }
 }
+
+
+
