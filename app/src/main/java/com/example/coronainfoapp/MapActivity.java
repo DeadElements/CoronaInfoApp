f70f494dcf02592deb0cf388ccaa991f5e952d0a
Manuel Dolenz <1910653532@stud.fh-kufstein.ac.at>
2021-02-28 09:44:17 +0100

    9 bl + fab

--- app/src/main/java/com/example/coronainfoapp/MapActivity.java
+++ app/src/main/java/com/example/coronainfoapp/MapActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -27,6 +28,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.internal.ManufacturerUtils;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -40,6 +42,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         hideNavigationbar();
+
+        FloatingActionButton fab4 = findViewById(R.id.go_back_to_rv);
+        fab4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MapActivity.this, RVActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.google_map);
@@ -118,6 +130,30 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap = googleMap;
         LatLng tirol = new LatLng(47.259659, 11.400375);
         mMap.addMarker(new MarkerOptions().position(tirol).title("Derzeit 1245 Aktive Fälle")
+                .icon(bitmapDescriptorFromVector(getApplicationContext(),R.drawable.ic_baseline_announcement_24)));
+        LatLng wien = new LatLng(48.210033, 	16.363449);
+        mMap.addMarker(new MarkerOptions().position(wien).title("Derzeit 8320 Aktive Fälle")
+                .icon(bitmapDescriptorFromVector(getApplicationContext(),R.drawable.ic_baseline_announcement_24)));
+        LatLng salzburg = new LatLng(47.811195, 13.033229);
+        mMap.addMarker(new MarkerOptions().position(salzburg).title("Derzeit 1613 Aktive Fälle")
+                .icon(bitmapDescriptorFromVector(getApplicationContext(),R.drawable.ic_baseline_announcement_24)));
+        LatLng niederoesterreich = new LatLng(48.2000, 15.6167);
+        mMap.addMarker(new MarkerOptions().position(niederoesterreich).title("Derzeit 5225 Aktive Fälle")
+                .icon(bitmapDescriptorFromVector(getApplicationContext(),R.drawable.ic_baseline_announcement_24)));
+        LatLng steiermark = new LatLng(47.359344, 14.469983);
+        mMap.addMarker(new MarkerOptions().position(steiermark).title("Derzeit 3093 Aktive Fälle")
+                .icon(bitmapDescriptorFromVector(getApplicationContext(),R.drawable.ic_baseline_announcement_24)));
+        LatLng burgenland = new LatLng(47.8456, 	16.5189);
+        mMap.addMarker(new MarkerOptions().position(burgenland).title("Derzeit 8362 Aktive Fälle")
+                .icon(bitmapDescriptorFromVector(getApplicationContext(),R.drawable.ic_baseline_announcement_24)));
+        LatLng oberoesterreich = new LatLng(48.3000, 14.2833);
+        mMap.addMarker(new MarkerOptions().position(oberoesterreich).title("Derzeit 3723 Aktive Fälle")
+                .icon(bitmapDescriptorFromVector(getApplicationContext(),R.drawable.ic_baseline_announcement_24)));
+        LatLng kaernten = new LatLng(46.6167, 14.3000);
+        mMap.addMarker(new MarkerOptions().position(kaernten).title("Derzeit 2126 Aktive Fälle")
+                .icon(bitmapDescriptorFromVector(getApplicationContext(),R.drawable.ic_baseline_announcement_24)));
+        LatLng vorarlberg = new LatLng(47.5050, 9.7492);
+        mMap.addMarker(new MarkerOptions().position(vorarlberg).title("Derzeit 4854 Aktive Fälle")
                 .icon(bitmapDescriptorFromVector(getApplicationContext(),R.drawable.ic_baseline_announcement_24)));
     }
     private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
